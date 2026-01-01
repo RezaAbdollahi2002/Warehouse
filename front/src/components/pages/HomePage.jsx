02b3e32b281fa602f6a5078b9352e1614aea9ddc
Reza <apa6323@psu.edu>
2026-01-01 16:07:00 -0500

    front is started

---
+++ front/src/components/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='flex flex-col'>
+      {/* Navbar */}
+      <div className='fixed top-0 px-1.5 py-1 bg-[#373190] text-white bg-blure-40 w-full'>
+        Hi
+      </div>
+      {/* Signin and signup */}
+      <div>
+
+      </div>
+      {/* Footer */}
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
